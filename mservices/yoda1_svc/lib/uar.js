5ecb0bd7258cafef6669d577355069e7eaeabc58
Oleg Tomin <2tominop@gmail.com>
2018-03-12 14:37:40 +0500

    update uar api sc youdex

--- mservices/yoda1_svc/lib/uar.js
+++ mservices/yoda1_svc/lib/uar.js
@@ -2,77 +2,63 @@ const uar = require("./uar_abi"); // address and ABI of UserAddrReg smart contra
 const uarContract = YODA3.eth.contract(uar.abi).at(uar.address); //  Dex smart contract in Youdex
 
 //  Route - newUser function
-app.get("/YODA/uar/newuser/:data", function (req, res, next) {
-    const data = YODA3.toHex((req.params.data).replace(/-/g, ''));
+app.get("/YODA/uar/newuser/:data", function(req, res, next) {
+    const data = '0x' + (req.params.data).replace(/-/g, '');
     console.log('uid: ' + data);
     makeYoudexTx('plasmoid', uar.address, 0, uarContract.newUser.getData(data), res, next);
 })
 
 //  Route - newAddr function
-app.get("/YODA/uar/newaddrs/:data", function (req, res, next) {
+app.get("/YODA/uar/newaddrs/:data", function(req, res, next) {
     const data = JSON.parse(req.params.data);
     if (data.symbol == 'ET') {
         if ((data.address).length != 42) next(new Error('Error: invalid ETH address string (<>42) (' + (data.address).length + ')'));
         else {
             data.symbol = YODA3.toHex(data.symbol);
         };
-    }
-    else if (data.symbol == 'BT' || data.symbol == 'B3') {
-        if ((data.address).length > 34 || (data.address).length < 26) next(new Error('Error: invalid BTC address string (<26, >42) (' + (data.address).length + ')'));
+    } else if (data.symbol == 'BT' || data.symbol == 'B3') {
+        const len = (data.address).length;
+        if (len > 34 || len < 26) next(new Error('Error: invalid BTC address string = ' + len.toString() + ',(must 26-34)'));
         else {
-            symbol = YODA3.toHex(data.symbol + (data.address).substring(20, (data.address).length));
+            //  n = 20 - 2 - (len - 20) = 40 - len - 2
+            data.symbol = YODA3.toHex(data.symbol + ('*').repeat(40 - len - 2) + (data.address).substring(20, len));
             data.address = YODA3.toHex((data.address).substring(0, 20));
         };
-    }
-    else next(new Error('Error: invalid currency symbol: ' + data.symbol));
-    data.uid = YODA3.toHex((data.uid).replace(/-/g, ''));
+    } else next(new Error('Error: invalid currency symbol: ' + data.symbol));
+    data.uid = '0x' + (data.uid).replace(/-/g, '');
     console.log('symbol: ' + data.symbol + ' addrs: ' + data.address + ' uid: ' + data.uid);
     makeYoudexTx('plasmoid', uar.address, 0, uarContract.newAddrs.getData(data.symbol, data.address, data.uid), res, next);
 })
 
 //  Route - setUserStatus function
-app.get("/YODA/uar/setuser/:data", function (req, res, next) {
-    if ((req.params.data).length == 35) {
-        const uid = (req.params.data).substring(0, 34),
-            status = parseInt((req.params.data).substring(34, 35));
+app.get("/YODA/uar/setuser/:data", function(req, res, next) {
+    if ((req.params.data).length == 38) {
+        var uid = (req.params.data).substring(0, 36),
+            status = parseInt((req.params.data).substring(37, 38));
+        uid = '0x' + (uid).replace(/-/g, '');
         makeYoudexTx('plasmoid', uar.address, 0, uarContract.setUserStatus.getData(uid, status), res, next);
-    }
-    else next(new Error('Error: length of data string not equal 35! (' + (req.params.data).length + ')'));
+    } else next(new Error('Error: length of data string not equal 38! (' + (req.params.data).length + ')'));
 })
 
 //  Route - checkUser function
-app.get("/YODA/uar/checkuser/:data", function (req, res, next) {
-    const data = YODA3.toHex((req.params.data).replace(/-/g, ''));
+app.get("/YODA/uar/checkuser/:data", function(req, res, next) {
+    const data = '0x' + (req.params.data).replace(/-/g, '');
     console.log('uid: ' + data);
-    uarContract.checkUser(data, function (error, result) {
+    uarContract.checkUser(data, function(error, result) {
         if (error) next(error)
-        else res.json(result);
+        else if (result[0] == 0) next(new Error('user not found'))
+        else res.json({ status: result[0], numAddrs: result[1] });
     });
 })
 
 //  Route - checkAddrs function
-app.get("/YODA/uar/checkaddrs/:data", function (req, res, next) {
-    uarContract.checkAddrs(req.params.data, function (error, result) {
+app.get("/YODA/uar/checkaddrs/:data", function(req, res, next) {
+    var symbol = (req.params.data).substring(0, 2),
+        address = (req.params.data).substring(2, (req.params.data).length);
+    console.log(symbol + '  ' + address);
+    if (symbol != 'ET') address = YODA3.toHex((address).substring(0, 20))
+    uarContract.checkAddrs(address, function(error, result) {
         if (error) next(error)
-        else res.json(result);
+        else res.json({ symbol: result[0], status: result[1], uid: result[2] });
     });
 })
-
-//  Route - outDepo function 
-app.get("/YODA/outUAR/:data", function (req, res, next) {
-    const order = parseInt(req.params.data),
-        myCallData = DExContract.outDepo.getData(order, plasmoid.ethAddrs);
-    makeYoudexTx('plasmoid', uar.address, 0, myCallData, res, next);
-})
-
-
-//  Route - startDex function 
-app.get("/YODA/startUAR/:data", function (req, res) {
-    const data = JSON.parse(req.params.data),
-        valueA = data.valueA, //  value of Alice's coins
-        valueB = data.valueB, //  value of Bob's coins
-        valueY = data.valueY; //  value of YODA tokens for pledge
-    orderID = 0;
-    myCallData = DExContract.openDEx.getData(alice.ethAddrs, plasmoid.ethAddrs, valueB * 10 ** 18, valueA * 10 ** 8, valueY * 10 ** 9);
-    makeYoudexTx('bob', uar.address, 0, myCallData, res);
-});
--- mservices/yoda1_svc/lib/uar_abi.js
+++ mservices/yoda1_svc/lib/uar_abi.js
@@ -1,175 +1,151 @@
 module.exports = {
-    'address': '0x48bf06f92de7853b1bb01be22deaad3c5969efd3',
-    'abi': [
-        {
+    'address': '0xc3e79faed95fc6c11ca3863b0659047e550db110',
+    'abi': [{
             "constant": true,
             "inputs": [],
             "name": "numUsers",
-            "outputs": [
-              {
+            "outputs": [{
                 "name": "",
                 "type": "uint8"
-              }
-            ],
+            }],
             "payable": false,
             "stateMutability": "view",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": true,
             "inputs": [],
             "name": "numAddrs",
-            "outputs": [
-              {
+            "outputs": [{
                 "name": "",
                 "type": "uint8"
-              }
-            ],
+            }],
             "payable": false,
             "stateMutability": "view",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": false,
-            "inputs": [
-              {
+            "inputs": [{
                 "name": "_uID",
-                "type": "bytes32"
-              }
-            ],
+                "type": "bytes16"
+            }],
             "name": "newUser",
-            "outputs": [
-              {
+            "outputs": [{
                 "name": "",
                 "type": "bool"
-              }
-            ],
+            }],
             "payable": false,
             "stateMutability": "nonpayable",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": false,
-            "inputs": [
-              {
-                "name": "_symbol",
-                "type": "bytes16"
-              },
-              {
-                "name": "_addrs",
-                "type": "address"
-              },
-              {
-                "name": "_uID",
-                "type": "bytes32"
-              }
+            "inputs": [{
+                    "name": "_symbol",
+                    "type": "bytes16"
+                },
+                {
+                    "name": "_addrs",
+                    "type": "address"
+                },
+                {
+                    "name": "_uID",
+                    "type": "bytes16"
+                }
             ],
             "name": "newAddrs",
-            "outputs": [
-              {
+            "outputs": [{
                 "name": "",
                 "type": "bool"
-              }
-            ],
+            }],
             "payable": false,
             "stateMutability": "nonpayable",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": false,
-            "inputs": [
-              {
-                "name": "_uID",
-                "type": "bytes32"
-              },
-              {
-                "name": "_status",
-                "type": "uint8"
-              }
+            "inputs": [{
+                    "name": "_uID",
+                    "type": "bytes16"
+                },
+                {
+                    "name": "_status",
+                    "type": "uint8"
+                }
             ],
             "name": "setUserStatus",
-            "outputs": [
-              {
+            "outputs": [{
                 "name": "",
                 "type": "bool"
-              }
-            ],
+            }],
             "payable": false,
             "stateMutability": "nonpayable",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": false,
-            "inputs": [
-              {
-                "name": "_addrs",
-                "type": "address"
-              },
-              {
-                "name": "_status",
-                "type": "uint8"
-              }
+            "inputs": [{
+                    "name": "_addrs",
+                    "type": "address"
+                },
+                {
+                    "name": "_status",
+                    "type": "uint8"
+                }
             ],
             "name": "setAddrsStatus",
-            "outputs": [
-              {
+            "outputs": [{
                 "name": "",
                 "type": "bool"
-              }
-            ],
+            }],
             "payable": false,
             "stateMutability": "nonpayable",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": true,
-            "inputs": [
-              {
+            "inputs": [{
                 "name": "_uID",
-                "type": "bytes32"
-              }
-            ],
+                "type": "bytes16"
+            }],
             "name": "checkUser",
-            "outputs": [
-              {
-                "name": "status",
-                "type": "uint8"
-              },
-              {
-                "name": "numaddrs",
-                "type": "uint8"
-              }
+            "outputs": [{
+                    "name": "status",
+                    "type": "uint8"
+                },
+                {
+                    "name": "numaddrs",
+                    "type": "uint8"
+                }
             ],
             "payable": false,
             "stateMutability": "view",
             "type": "function"
-          },
-          {
+        },
+        {
             "constant": true,
-            "inputs": [
-              {
+            "inputs": [{
                 "name": "_addrs",
                 "type": "address"
-              }
-            ],
+            }],
             "name": "checkAddrs",
-            "outputs": [
-              {
-                "name": "symbol",
-                "type": "bytes16"
-              },
-              {
-                "name": "status",
-                "type": "uint8"
-              },
-              {
-                "name": "uID",
-                "type": "bytes32"
-              }
+            "outputs": [{
+                    "name": "symbol",
+                    "type": "bytes16"
+                },
+                {
+                    "name": "status",
+                    "type": "uint8"
+                },
+                {
+                    "name": "uID",
+                    "type": "bytes16"
+                }
             ],
             "payable": false,
             "stateMutability": "view",
             "type": "function"
-          }
-                ]
+        }
+    ]
 }
