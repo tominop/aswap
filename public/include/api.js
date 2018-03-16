ccf637b87d6b687f554c441f01d5ac9f0bb7283b
Oleg Tomin <2tominop@gmail.com>
2018-03-16 08:39:53 +0500

    global update aswpa

---
+++ public/include/api.js
@@ -0,0 +1,6 @@
+        //  microservices & hosts addresses 
+        var //hostBtcApi = 'http://localhost:8100/btc/', //  main net
+            hostBtcApi = 'http://localhost:8103/btc3/', // testnet3
+            hostEthApi = 'http://localhost:8200/eth/',
+            hostYODAApi = 'http://localhost:8201/YODA/',
+            hostPrice = 'https://api.coinmarketcap.com/v1/ticker';
